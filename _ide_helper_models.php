4b362abe19bf6d00f8d389effd127e003543dfa6
eltoro0815 <th_ru@web.de>
2019-02-03 23:52:47 +0100

    clean up

---
+++ _ide_helper_models.php
@@ -0,0 +1,61 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App{
+/**
+ * App\Crud
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $color
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud whereColor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Crud whereUpdatedAt($value)
+ */
+	class Crud extends \Eloquent {}
+}
+
+namespace App{
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
