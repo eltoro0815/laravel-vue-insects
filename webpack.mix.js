782c4eeeab7a3ed0ecc94c6a76dede3cc46cb2df
eltoro0815 <th_ru@web.de>
2019-02-04 22:02:12 +0100

    corrected laravel-mix with browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,10 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .version();
+   .sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync({
-   proxy: 'cruds.ddev.local',
-   host: 'cruds.ddev.local'
+   proxy: 'insects.ddev.local',
+   host: 'insects.ddev.local'
 });
