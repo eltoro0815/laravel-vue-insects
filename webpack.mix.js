d0aedbcad31464234a12b3f28ebd4c9acc820206
eltoro0815 <th_ru@web.de>
2019-02-05 20:07:14 +0100

    added source maps for better javascript debugging

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js').sourceMaps();
+mix.sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync({
    proxy: 'insects.ddev.local',
