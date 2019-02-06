016677cf91cb88b0cd27ed92b069c74a1d365853
eltoro0815 <th_ru@web.de>
2019-02-06 01:42:19 +0100

    integrated draggable component in App

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,106 +1,107 @@
 <template>
-  <div>
-    <div class="container mt-4">
-      <h2>You find {{insects.length | insects_unit}} in this spot.</h2>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col d-flex">
-          <div class="row border align-items-end mt-4 ml-0 mr-0">
-            <img src="images/random_400x400.png" alt="random" style="width:100%;max-width:400px;" class="p-2 mx-auto d-block">
-            <button type="button" class="btn btn-primary btn-block m-2" @click="create">Add a random insect</button>
-          </div>
+    <div>
+        <div class="container mt-4">
+            <h2>You find {{insects.length | insects_unit}} in this spot.</h2>
+            <button @click="create_insect" class="btn btn-primary" type="button">Add a random insect</button>
         </div>
-
-        <insect-component
-          v-for="insect in insects"
-          v-bind="insect"
-          :key="insect.id"
-          @update="update"
-          @delete="del"
-        ></insect-component>
-      </div>
+        <div class="container">
+            <draggable :list="insects" :options="{draggable:'.item', animation:200, handle:'.my-handle'}" class="row">
+                <insect-component
+                        class="item"
+                        v-for="insect in insects"
+                        v-bind="insect"
+                        :key="insect.id"
+                        @update_insect="update_insect"
+                        @delete_insect="delete_insect"
+                ></insect-component>
+            </draggable>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-function Insect({ id, type, name, position }) {
-  this.id = id;
-  this.type = type;
-  this.name = name;
-  this.position = position;
-}
-import InsectComponent from "./InsectComponent.vue";
-export default {
-  data() {
-    return {
-      insects: [],
-      mute: false
+    function Insect({id, type, name, position}) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.position = position;
+    }
+
+    import InsectComponent from "./InsectComponent.vue";
+    import draggable from 'vuedraggable'
+
+    export default {
+        components: {
+            InsectComponent,
+            draggable
+        },
+
+        data() {
+            return {
+                insects: [],
+                mute: false
+            };
+        },
+
+        filters: {
+            insects_unit(value) {
+                switch (value) {
+                    case 0:
+                        return "no insects";
+                        break;
+                    case 1:
+                        return "1 insect";
+                        break;
+                    default:
+                        return value + " insects";
+                        break;
+                }
+            }
+        },
+
+        methods: {
+
+            create_insect() {
+                this.mute = true;
+                window.axios.get("/api/insects/create").then(({data}) => {
+                    this.insects.push(new Insect(data));
+                    this.mute = false;
+                });
+            },
+            read() {
+                this.mute = true;
+                window.axios.get("/api/insects").then(({data}) => {
+                    data.forEach(insect => {
+                        this.insects.push(new Insect(insect));
+                    });
+                    this.mute = false;
+                });
+            },
+            update_insect(id, type) {
+                this.mute = true;
+                window.axios.put(`/api/insects/${id}`, {type}).then(() => {
+                    this.insects.find(insect => insect.id === id).type = type;
+                    this.mute = false;
+                });
+            },
+            delete_insect(id) {
+                this.mute = true;
+                window.axios.delete(`/api/insects/${id}`).then(() => {
+                    let index = this.insects.findIndex(insect => insect.id === id);
+                    this.insects.splice(index, 1);
+                    this.mute = false;
+                });
+            }
+        },
+        watch: {
+            mute(value) {
+                // %todo%
+            }
+        },
+
+        created() {
+            this.read();
+        }
     };
-  },
-
-  filters: {
-    insects_unit(value) {
-      switch (value) {
-        case 0:
-          return "no insects";
-          break;
-        case 1:
-          return "1 insect";
-          break;
-        default:
-          return value + " insects";
-          break;
-      }
-    }
-  },
-
-  methods: {
-
-    create() {
-      this.mute = true;
-      window.axios.get("/api/insects/create").then(({ data }) => {
-        this.insects.push(new Insect(data));
-        this.mute = false;
-      });
-    },
-    read() {
-      this.mute = true;
-      window.axios.get("/api/insects").then(({ data }) => {
-        data.forEach(insect => {
-          this.insects.push(new Insect(insect));
-        });
-        this.mute = false;
-      });
-    },
-    update(id, type) {
-      this.mute = true;
-      window.axios.put(`/api/insects/${id}`, { type }).then(() => {
-        this.insects.find(insect => insect.id === id).type = type;
-        this.mute = false;
-      });
-    },
-    del(id) {
-      this.mute = true;
-      window.axios.delete(`/api/insects/${id}`).then(() => {
-        let index = this.insects.findIndex(insect => insect.id === id);
-        this.insects.splice(index, 1);
-        this.mute = false;
-      });
-    }
-  },
-  watch: {
-    mute(value) {
-      // %todo%
-    }
-  },
-  components: {
-    InsectComponent: InsectComponent
-  },
-  created() {
-    this.read();
-  }
-};
 </script>
 
